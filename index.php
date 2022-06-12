2d421b7e953d184b1db43723e5164e657b401061
anggrainid <70145334+anggrainid@users.noreply.github.com>
2022-06-12 14:30:45 +0700

    Add files via upload
    pertama bgt

---
+++ index.php
@@ -0,0 +1,192 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Yuk Laundry!</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+        <!-- Font Awesome icons (free version)-->
+        <script src="https://use.fontawesome.com/releases/v6.1.0/js/all.js" crossorigin="anonymous"></script>
+        <!-- Google fonts-->
+        <link href="https://fonts.googleapis.com/css?family=Varela+Round" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet" />
+        <!-- Core theme CSS (includes Bootstrap)-->
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-light fixed-top" id="mainNav">
+            <div class="container px-4 px-lg-5">
+                <a class="navbar-brand" href="#page-top">Beranda</a>
+                <button class="navbar-toggler navbar-toggler-right" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
+                    Menu
+                    <i class="fas fa-bars"></i>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ms-auto">
+                        <li class="nav-item"><a class="nav-link" href="#keunggulan">Keunggulan</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#daftar_laundry">Daftar Laundry</a></li>
+                        <li class="nav-item"><a class="nav-link" href="#cek_ongkir">Cek Ongkir</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <!-- Masthead-->
+        <header class="masthead">
+            <div class="container px-4 px-lg-5 d-flex h-100 align-items-center justify-content-center">
+                <div class="d-flex justify-content-center">
+                    <div class="text-center">
+                        <h1 class="mx-auto my-0 text-uppercase">Yuk Laundry</h1>
+                        <h2 class="text-white-50 mx-auto mt-2 mb-5">Website yang menyediakan data-data tempat Laundry di seluruh Indonesia untuk memudahkan kamu mencari Laundry terdekat.</h2>
+                        <a class="btn btn-primary" href="#tempat_wisata">Lihat Daftar Tempat Laundry</a>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <!-- About
+        <section class="about-section text-center" id="about">
+            <div class="container px-4 px-lg-5">
+                <div class="row gx-4 gx-lg-5 justify-content-center">
+                    <div class="col-lg-8">
+                        <h2 class="text-white mb-4">Built with Bootstrap 5</h2>
+                        <p class="text-white-50">
+                            Grayscale is a free Bootstrap theme created by Start Bootstrap. It can be yours right now, simply download the template on
+                            <a href="https://startbootstrap.com/theme/grayscale/">the preview page.</a>
+                            The theme is open source, and you can use it for any purpose, personal or commercial.
+                        </p>
+                    </div>
+                </div>
+                <img class="img-fluid" src="assets/img/ipad.png" alt="..." />
+            </div>
+        </section>
+        -->
+        <!-- Projects-->
+        <section class="projects-section bg-light" id="keunggulan">
+            <div class="container px-4 px-lg-5">
+                <!-- Featured Project Row
+                <div class="row gx-0 mb-4 mb-lg-5 align-items-center">
+                    <div class="col-xl-8 col-lg-7"><img class="img-fluid mb-3 mb-lg-0" src="assets/img/bg-masthead.jpg" alt="..." /></div>
+                    <div class="col-xl-4 col-lg-5">
+                        <div class="featured-text text-center text-lg-left">
+                            <h4>Shoreline</h4>
+                            <p class="text-black-50 mb-0">Grayscale is open source and MIT licensed. This means you can use it for any project - even commercial projects! Download it, customize it, and publish your website!</p>
+                        </div>
+                    </div>
+                </div>
+                -->
+                <!-- Project One Row-->
+                <div class="row gx-0 mb-5 mb-lg-0 justify-content-center">
+                    <div class="col-lg-6"><a href="detail-muara_beting.php"><img class="img-fluid" width="700px" height="500px" src="assets/img/laundry3.jpg" alt="..." /></a></div>
+                    <div class="col-lg-6">
+                        <div class="bg-black text-center h-100 project">
+                            <div class="d-flex h-100">
+                                <div class="project-text w-100 my-auto text-center text-lg-left">
+                                    <h4 class="text-white">Terbaik</h4>
+                                    <p class="mb-0 text-white-50">Data tempat Laundry terbaik di seluruh Indonesia</p>
+                                    <hr class="d-none d-lg-block mb-0 ms-0" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Project Two Row-->
+                <div class="row gx-0 justify-content-center">
+                    <div class="col-lg-6"><a href="detail-gowet.php"><img class="img-fluid" width="700px" height="500px" src="assets/img/laundry1.jpg" alt="..." /></a></div>
+                    <div class="col-lg-6 order-lg-first">
+                        <div class="bg-black text-center h-100 project">
+                            <div class="d-flex h-100">
+                                <div class="project-text w-100 my-auto text-center text-lg-right">
+                                    <h4 class="text-white">Terbaru</h4>
+                                    <p class="mb-0 text-white-50">Data tempat Laundry selalu diperbaharui.</p>
+                                    <hr class="d-none d-lg-block mb-0 me-0" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+                <!-- Project Three Row-->
+                <div class="row gx-0 mb-5 mb-lg-0 justify-content-center">
+                    <div class="col-lg-6"><a href="detail-jatiasih.php"><img class="img-fluid" width="700px" height="500px" src="assets/img/laundry2.jpg" alt="..." /></a></div>
+                    <div class="col-lg-6">
+                        <div class="bg-black text-center h-100 project">
+                            <div class="d-flex h-100">
+                                <div class="project-text w-100 my-auto text-center text-lg-left">
+                                    <h4 class="text-white">Terlengkap</h4>
+                                    <p class="mb-0 text-white-50">Menyediakan data-data tempat Laundry selalu terlengkap.</p>
+                                    <hr class="d-none d-lg-block mb-0 ms-0" />
+                                </div>
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- Daftar Harga -->
+        <section class="about-section text-center" id="daftar_laundry" style="padding-top:5rem; padding-bottom:5rem;">
+            <div class="container px-4 px-lg-5 d-flex h-100 align-items-center justify-content-center">
+                <div class="row gx-4 gx-lg-5 justify-content-center">
+                    <div class="col-lg-8">
+                        <h2 class="text-white mb-4">Daftar Tempat Laundry</h2>
+                        <p class="text-white-50 mb-4">
+                            Nah, berikut ini daftar tempat laundry di seluruh Indonesia untuk memudahkan kamu mencari tempat laundry terdekat!
+                        </p>
+                        <table class="table table-bordered">
+                            <thead class="text-white-50">
+                              <tr>
+                                <th scope="col">No</th>
+                                <th scope="col">Nama</th>
+                                <th scope="col">Alamat</th>
+                                <th scope="col">Telepon</th>
+                              </tr>
+                            </thead>
+                            <?php
+                            include "koneksi.php";
+                            $data = mysqli_query($konek, "SELECT * FROM tbl_laundry");
+                            $no = 1;
+                            foreach ($data as $row){
+                                echo "<tbody class='text-white-50'><tr>
+                                  <th scope='row'>$no</th>
+                                  <td>" . $row['nama'] . "</td>
+                                  <td>" . $row['alamat'] . "</td>
+                                  <td>" . $row['telepon'] . "</td>
+                                    </tr></tbody>";
+                                        $no++;
+                            }
+                            ?>
+
+                          </table>
+                    </div>
+                    
+                </div>
+            </div>
+        </section>
+
+        <!-- Form Pemesanan
+
+        <section class="projects-section bg-light" id="cek_ongkir">
+            <div class="container px-4 px-lg-5">
+                <div class="row gx-4 gx-lg-5 justify-content-center">
+                    <div class="col-lg-8 col-xl-6 text-center">
+                        <a href="form-input.php" class="btn btn-primary" role="button">Cek Ongkir</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+         -->
+        
+        <!-- Signup-->
+        <!-- Footer-->
+        <footer class="footer bg-black small text-center text-white-50"><div class="container px-4 px-lg-5">Copyright &copy; Anggraini D 2022</div></footer>
+        <!-- Bootstrap core JS-->
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+        <!-- * *                               SB Forms JS                               * *-->
+        <!-- * * Activate your form at https://startbootstrap.com/solution/contact-forms * *-->
+        <!-- * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * * *-->
+        <script src="https://cdn.startbootstrap.com/sb-forms-latest.js"></script>
+    </body>
+</html>
